2f614bd6120ae9801da721895700733ab668dcc7
shraddhashivakumar <162862002+shraddhashivakumar@users.noreply.github.com>
2026-03-10 23:20:32 +0530

    Final commit

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ class Drives(db.Model):
     location = db.Column(db.String(100), nullable = False)
     deadline = db.Column(db.DateTime, nullable = False)
     status = db.Column(db.Boolean, default=False) #open or closed
+    is_rejected = db.Column(db.Boolean, default=False)
 
 class Applications(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -52,10 +53,12 @@ class Applications(db.Model):
 
 
 with app.app_context(): 
-    db.create_all()   
-    users = Users(username='admin', password='admin123', role='admin')
-    db.session.add(users)
-    db.session.commit()
+    db.create_all()
+    admin = Users.query.filter_by(username='admin').first()
+    if not admin:
+        users = Users(username='admin', password='admin123', role='admin')
+        db.session.add(users)
+        db.session.commit()   
 
 #index page
 @app.route("/", methods = ['GET', 'POST'])
@@ -132,8 +135,21 @@ def admin_dashboard():
     if'user_id' not in session or session['role']!='admin':
         flash('Access not allowed. Contact admin.')
         return redirect('/login')
-    companies = Companies.query.all()
-    students = Students.query.all()
+    
+    student_search = request.args.get('student_search')
+    company_search = request.args.get('company_search')
+
+    if student_search:
+        students = Students.query.filter(Students.name.ilike(f"%{student_search}%") | Students.id.ilike(f"%{student_search}%")).all()
+    else:
+        students = Students.query.all()
+
+    
+    if company_search:
+        companies = Companies.query.filter(Companies.company_name.ilike(f"%{company_search}%")).all()
+    else:
+        companies = Companies.query.all()
+
     applications = Applications.query.all()
     drives = Drives.query.all()
     total_drives = len(drives)
@@ -159,7 +175,7 @@ def approve_company(company_id):
         flash('Company not found')
     return redirect('/admin_dashboard')
 
-
+#reject company
 @app.route('/admin/reject_company/<int:company_id>', methods = ['POST'])
 def reject_company(company_id):
     if 'user_id' not in session or session['role'] != 'admin':
@@ -174,6 +190,7 @@ def reject_company(company_id):
         flash('Company not found')
     return redirect('/admin_dashboard')
 
+#blacklist company
 @app.route('/admin/blacklist_company/<int:company_id>', methods = ['POST'])
 def blacklist_company(company_id):
     if 'user_id' not in session or session['role'] != 'admin':
@@ -226,6 +243,21 @@ def mark_as_complete(drive_id):
         flash("Drive marked as complete.")
     return redirect('/admin_dashboard')
 
+#reject drives 
+@app.route('/admin/reject_drive/<int:drive_id>', methods=['POST'])
+def reject_drive(drive_id):
+    if 'user_id' not in session or session['role'] != 'admin':
+        flash("Access not allowed.")
+        return redirect('/login')
+    
+    drive = Drives.query.get(drive_id)
+    if drive:
+        drive.is_rejected = True
+        db.session.commit()
+        flash("Drive rejected successfully")
+    else:
+        flash("Drive not found")
+    return redirect('/admin_dashboard')
 
 
 #________________________________________________________________________________________________________________
@@ -237,6 +269,10 @@ def company_dashboard():
         flash('Access not allowed, contact admin.')
         return redirect('/login')
     company = Companies.query.filter_by(user_id = session['user_id']).first()
+    if company.is_blacklisted:
+        flash("Your company has been blacklisted by admin.")
+        return redirect('/login')
+
     if not company:
         flash("Company profile not found. Please complete signup again.")
         return redirect('/login')
@@ -246,6 +282,12 @@ def company_dashboard():
 #create drives 
 @app.route('/create_drive', methods=['GET','POST'])
 def create_drive():
+    company = Companies.query.filter_by(user_id = session['user_id']).first()
+    
+    if not company.status:
+        flash("Admin approval pending...")
+        return redirect('/company_dashboard')
+
     if request.method == 'POST':
         drive_name = request.form.get('drive_name')
         job_title = request.form.get('job_title')
@@ -254,9 +296,8 @@ def create_drive():
         location = request.form.get('location')
         deadline = request.form.get('deadline')
         deadline = datetime.strptime(deadline, '%Y-%m-%dT%H:%M')
-        #id = session['user_id']
         company = Companies.query.filter_by(user_id=session['user_id']).first()
-        drive = drive = Drives( company_id=company.id,
+        drive = Drives( company_id=company.id,
                                 drive_name=drive_name,
                                 job_title=job_title,
                                 job_description=job_description,
@@ -266,6 +307,7 @@ def create_drive():
         db.session.add(drive)
         db.session.commit()
         return redirect('/company_dashboard')
+    return render_template("create_drive.html")
 
 @app.route('/edit_drive/<int:drive_id>', methods = ['POST', 'GET'])
 def edit_drive(drive_id):
@@ -274,7 +316,6 @@ def edit_drive(drive_id):
         return "Drive not found"
     
     drive.drive_name = request.form.get('drive_name')
-    drive.drive_name=request.form.get('drive_name')
     drive.job_title=request.form.get('job_title')
     drive.job_description=request.form.get('job_description')
     drive.salary=request.form.get('salary')
@@ -309,11 +350,17 @@ def update_application(app_id, status):
 def student_dashboard():
     if 'user_id' not in session or session['role']!='student':
         return redirect('/login')
+    
     student = Students.query.filter_by(user_id=session['user_id']).first()
     if not student:
-        flash("Student profile not found. Please complete signup again.")
+        flash("Student profile not found.")
         return redirect('/login')
-    drives = Drives.query.filter_by(status=False).all()
+
+    if student.is_blacklisted:
+        flash("You are blacklisted and cannot access the portal.")
+        return redirect('/login')
+    
+    drives = Drives.query.filter_by(status=False, is_rejected=False).all()
     applications = Applications.query.filter_by(student_id=student.id).all()
     return render_template('student_dashboard.html', drives=drives, applications=applications, student=student)
 
@@ -323,8 +370,13 @@ def student_dashboard():
 def apply(drive_id):
     if 'user_id' not in session or session['role']!='student':
         return redirect('/login')
-
+ 
     student = Students.query.filter_by(user_id=session['user_id']).first()
+
+    if student.is_blacklisted:
+        flash("You are blacklisted and cannot access the portal.")
+        return redirect('/login')
+
     existing = Applications.query.filter_by(student_id=student.id, drive_id=drive_id).first()
     if existing:
         flash("Already applied")
@@ -351,6 +403,7 @@ def edit_profile(id):
         flash("Profile updated successfully")
         return redirect('/student_dashboard')
     return render_template('edit_profile.html', student=student)
+
 
 
 if __name__=="__main__":
